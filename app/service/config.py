59759d0bdacc4366901e3342bd4b4df3be06d355
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2023-12-26 01:05:13 +0000

    implement service router

---
+++ app/service/config.py
@@ -0,0 +1,1 @@
+ICS_FILE_PATH = "ics_files"
--- app/service/router.py
+++ app/service/router.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FileResponse, BackgroundTask
 import os
 
 from app.trip.Trip import Trip
 from app.service.ICSGenerator import ICSGenerator
+from app.service import config
 
 router = APIRouter()
 
 @router.post("/trip/generate_ics")
 async def create_event(trip : Trip):
-    pass
-    ics_file_path = ICSGenerator.generate_ics(trip)
-    # response = FileResponse(path=ics_file_path, filename="event.ics", media_type='text/calendar')
+    ics_file_name = ICSGenerator.generate_ics(trip)
+    path = f'{config.ICS_FILE_PATH}/{ics_file_name}'
+    response = FileResponse(path=path, filename=ics_file_name, media_type='text/calendar')
 
-    # # Delete the temporary file after sending the response
-    # response.background = BackgroundTask(os.unlink, ics_file_path)
-    # return response
+    # Delete the temporary file after sending the response
+    response.background = BackgroundTask(os.unlink, path)
+    return response
