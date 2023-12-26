686b2303703bd83b03b3fb7c3988518a171c1314
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2023-12-26 00:40:24 +0000

    refactor test_Event

--- tests/trip/test_Event.py
+++ tests/trip/test_Event.py
@@ -3,7 +3,8 @@ import pytest
 
 from app.trip.Event import Event
 
-def test_valid_event():
+@pytest.fixture
+def generated_event_data():
     event_data = {
         "title": "Sample Event",
         "start_time": datetime(2023, 1, 1, 12, 0),
@@ -12,7 +13,11 @@ def test_valid_event():
         "location": "Sample Location",
         "description": "Sample Description",
     }
-    event = Event(**event_data)
+    return event_data
+
+def test_valid_event(generated_event_data):
+    
+    event = Event(**generated_event_data)
     assert event.title == "Sample Event"
     assert event.start_time == datetime(2023, 1, 1, 12, 0)
     assert event.end_time == datetime(2023, 1, 1, 14, 0)
@@ -20,68 +25,32 @@ def test_valid_event():
     assert event.location == "Sample Location"
     assert event.description == "Sample Description"
 
-def test_event_missing_title():
+def test_event_missing_title(generated_event_data):
+    del generated_event_data["title"]
     with pytest.raises(ValueError):
-        event_data = {
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "timezone": "UTC",
-            "location": "Sample Location",
-            "description": "Sample Description",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
 
-def test_event_missing_start_time():
+def test_event_missing_start_time(generated_event_data):
+    del generated_event_data["start_time"]
     with pytest.raises(ValueError):
-        event_data = {
-            "title": "Sample Event",
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "timezone": "UTC",
-            "location": "Sample Location",
-            "description": "Sample Description",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
 
-def test_event_missing_end_time():
+def test_event_missing_end_time(generated_event_data):
+    del generated_event_data["end_time"]
     with pytest.raises(ValueError):
-        event_data = {
-            "title": "Sample Event",
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "timezone": "UTC",
-            "location": "Sample Location",
-            "description": "Sample Description",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
 
-def test_event_missing_timezone():
+def test_event_missing_timezone(generated_event_data):
+    del generated_event_data["timezone"]
     with pytest.raises(ValueError):
-        event_data = {
-            "title": "Sample Event",
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "location": "Sample Location",
-            "description": "Sample Description",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
 
-def test_event_missing_location():
+def test_event_missing_location(generated_event_data):
+    del generated_event_data["location"]
     with pytest.raises(ValueError):
-        event_data = {
-            "title": "Sample Event",
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "timezone": "UTC",
-            "description": "Sample Description",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
 
-def test_event_missing_description():
+def test_event_missing_description(generated_event_data):
+    del generated_event_data["description"]
     with pytest.raises(ValueError):
-        event_data = {
-            "title": "Sample Event",
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "timezone": "UTC",
-            "location": "Sample Location",
-        }
-        event = Event(**event_data)
+        event = Event(**generated_event_data)
