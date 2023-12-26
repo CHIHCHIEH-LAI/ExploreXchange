fa42ddf32c8ccfce96170c02b959d7b52c8ea608
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2023-12-26 01:04:43 +0000

    refactor test_Trip

--- tests/trip/test_Trip.py
+++ tests/trip/test_Trip.py
@@ -2,32 +2,29 @@ from datetime import datetime
 import pytest
 
 from app.trip.Trip import Trip
-from app.trip.Event import Event
 
 def test_valid_trip():
-    event_data1 = {
-        "title": "Event 1",
-        "start_time": datetime(2023, 1, 1, 12, 0),
-        "end_time": datetime(2023, 1, 1, 14, 0),
-        "timezone": "UTC",
-        "location": "Location 1",
-        "description": "Description 1",
-    }
-    
-    event_data2 = {
-        "title": "Event 2",
-        "start_time": datetime(2023, 1, 2, 12, 0),
-        "end_time": datetime(2023, 1, 2, 14, 0),
-        "timezone": "UTC",
-        "location": "Location 2",
-        "description": "Description 2",
-    }
-    
     trip_data = {
         "trip_name": "Sample Trip",
-        "events": [Event(**event_data1), Event(**event_data2)]
+        "events": [
+            {
+                "title": "Event 1",
+                "start_time": datetime(2023, 1, 1, 12, 0),
+                "end_time": datetime(2023, 1, 1, 14, 0),
+                "timezone": "UTC",
+                "location": "Location 1",
+                "description": "Description 1",
+            },
+            {
+                "title": "Event 2",
+                "start_time": datetime(2023, 1, 2, 12, 0),
+                "end_time": datetime(2023, 1, 2, 14, 0),
+                "timezone": "UTC",
+                "location": "Location 2",
+                "description": "Description 2",
+            }
+        ]
     }
-    
     trip = Trip(**trip_data)
     
     assert trip.trip_name == "Sample Trip"
@@ -35,28 +32,26 @@ def test_valid_trip():
 
 def test_trip_missing_trip_name():
     with pytest.raises(ValueError):
-        event_data1 = {
-            "title": "Event 1",
-            "start_time": datetime(2023, 1, 1, 12, 0),
-            "end_time": datetime(2023, 1, 1, 14, 0),
-            "timezone": "UTC",
-            "location": "Location 1",
-            "description": "Description 1",
-        }
-        
-        event_data2 = {
-            "title": "Event 2",
-            "start_time": datetime(2023, 1, 2, 12, 0),
-            "end_time": datetime(2023, 1, 2, 14, 0),
-            "timezone": "UTC",
-            "location": "Location 2",
-            "description": "Description 2",
-        }
-        
         trip_data = {
-            "events": [Event(**event_data1), Event(**event_data2)]
+            "events": [
+                {
+                    "title": "Event 1",
+                    "start_time": datetime(2023, 1, 1, 12, 0),
+                    "end_time": datetime(2023, 1, 1, 14, 0),
+                    "timezone": "UTC",
+                    "location": "Location 1",
+                    "description": "Description 1",
+                },
+                {
+                    "title": "Event 2",
+                    "start_time": datetime(2023, 1, 2, 12, 0),
+                    "end_time": datetime(2023, 1, 2, 14, 0),
+                    "timezone": "UTC",
+                    "location": "Location 2",
+                    "description": "Description 2",
+                }
+            ]
         }
-        
         trip = Trip(**trip_data)
 
 def test_trip_missing_events():
