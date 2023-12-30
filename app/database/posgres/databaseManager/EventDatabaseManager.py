5961988844e9bd25aee79ff8daec7d30835eaf56
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2023-12-30 19:08:10 +0000

    add database manager

---
+++ app/database/posgres/databaseManager/EventDatabaseManager.py
@@ -0,0 +1,5 @@
+from app.database.posgres.databaseManager.BaseDatabaseManager import BaseDatabaseManager
+
+class EventDatabaseManager(BaseDatabaseManager):
+    def create_event(self):
+        pass
---
+++ app/database/posgres/databaseManager/TripDatabaseManager.py
@@ -0,0 +1,5 @@
+from app.database.posgres.databaseManager.BaseDatabaseManager import BaseDatabaseManager
+
+class TripDatabaseManager(BaseDatabaseManager):
+    def create_trip(self, trip_data):
+        pass
