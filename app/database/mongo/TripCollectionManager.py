b41c9db6f11fd170b4466fdf423d97b6e4dcb74c
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2023-12-31 12:41:49 +0000

    implement trip collection mgr

--- app/database/mongo/TripCollectionManager.py
+++ app/database/mongo/TripCollectionManager.py
@@ -31,38 +31,38 @@ class TripCollectionManager(CollectionManager):
         query = {'_id': ObjectId(trip_id)}
         self.col.delete_one(query)
 
-from app.models.Event import Event
-from datetime import datetime
-from app.database.mongo.config import MONGODB_URI, DATABASE
-from app.database.mongo.DatabaseManager import DatabaseManager
+# from app.models.Event import Event
+# from datetime import datetime
+# from app.database.mongo.config import MONGODB_URI, DATABASE
+# from app.database.mongo.DatabaseManager import DatabaseManager
 
-dbMgr = DatabaseManager(MONGODB_URI, DATABASE)
-colMgr = TripCollectionManager(dbMgr, 'trips')
+# dbMgr = DatabaseManager(MONGODB_URI, DATABASE)
+# colMgr = TripCollectionManager(dbMgr, 'trips')
 
-trip = Trip(
-    title="Mountain Adventure",
-    start_time=datetime(2023, 1, 1, 9, 0),
-    end_time=datetime(2023, 1, 4, 10, 0),
-    location="Mountains",
-    description="A trip to the mountains",
-    owner="Alice",
-    events=[
-        Event(
-            title="Hiking",
-            start_time=datetime(2023, 1, 1, 10, 0),
-            end_time=datetime(2023, 1, 1, 12, 0),
-            description="A trip to the mountains",
-            owner="Alice"
-        ),
-        Event(
-            title="Camping",
-            start_time=datetime(2023, 1, 2, 15, 0),
-            end_time=datetime(2023, 1, 3, 10, 0),
-            location="Campground",
-            owner="Alice"
-        )
-    ]
-)
+# trip = Trip(
+#     title="Mountain Adventure",
+#     start_time=datetime(2023, 1, 1, 9, 0),
+#     end_time=datetime(2023, 1, 4, 10, 0),
+#     location="Mountains",
+#     description="A trip to the mountains",
+#     owner="Alice",
+#     events=[
+#         Event(
+#             title="Hiking",
+#             start_time=datetime(2023, 1, 1, 10, 0),
+#             end_time=datetime(2023, 1, 1, 12, 0),
+#             description="A trip to the mountains",
+#             owner="Alice"
+#         ),
+#         Event(
+#             title="Camping",
+#             start_time=datetime(2023, 1, 2, 15, 0),
+#             end_time=datetime(2023, 1, 3, 10, 0),
+#             location="Campground",
+#             owner="Alice"
+#         )
+#     ]
+# )
 
 # colMgr.create_trip(trip)
 # docs = colMgr.query_all_trips_of_owner('Alice')
