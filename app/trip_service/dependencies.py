a7e79a128c05e6abdb8eb045c74e68121951d3dc
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-01-01 16:26:41 +0000

    refactor code

--- app/trip_service/dependencies.py
+++ app/trip_service/dependencies.py
@@ -1,4 +1,4 @@
-from app.trip_service.config import MONGODB_URI, DATABASE
+from app.trip_service.config import MONGODB_URI, DATABASE, TRIPS_COLLECTION
 from app.database.mongo.database_manager import DatabaseManager
 from app.trip_service.utils.trip_collection_manager import TripCollectionManager
 from app.trip_service.utils.trip_to_ics_converter import TripToICSConverter
@@ -6,7 +6,7 @@ from app.trip_service.utils.ics_file_saver import ICSFileSaver
 
 def get_trip_collection_manager():
     dbMgr = DatabaseManager(MONGODB_URI, DATABASE)
-    colMgr = TripCollectionManager(dbMgr, 'trips')
+    colMgr = TripCollectionManager(dbMgr, TRIPS_COLLECTION)
     return colMgr
 
 def get_trip_to_ics_converter():
