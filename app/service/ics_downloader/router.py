9d63fe4b6bdc59107357d069d646918a778d5fde
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-01-01 13:32:49 +0000

    rename file

--- app/service/ics_downloader/router.py
+++ app/service/ics_downloader/router.py
@@ -5,7 +5,7 @@ import os
 from app.database.mongo.config import MONGODB_URI, DATABASE
 from app.service.ics_downloader.config import ICS_DIR_PATH
 from app.database.mongo.database_manager import DatabaseManager
-from app.database.mongo.trip_collection_anager import TripCollectionManager
+from app.database.mongo.trip_collection_manager import TripCollectionManager
 from app.service.ics_conversion.trip_to_ics_converter import TripToICSConverter
 from app.service.ics_conversion.ics_file_saver import ICSFileSaver
 
