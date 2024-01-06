9205f410108f56affb8e830a1b45139294ca0558
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-01-06 17:11:12 +0000

    pass trip to backend in trip format

--- app/trip_service/router.py
+++ app/trip_service/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, BackgroundTasks, status, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import FileResponse
+from fastapi.responses import RedirectResponse, FileResponse
 import os
 
 from app.common.models.trip import Trip
@@ -22,54 +22,58 @@ templates = Jinja2Templates(directory=FRONTEND_TEMPLATE_DIR)
 async def plan_trip(request: Request):
     return templates.TemplateResponse("trip_planning.html", {"request": request})
 
-@router.delete("/clear-trips", status_code=status.HTTP_204_NO_CONTENT)
-async def clear_trips(
-    tripMgr = Depends(get_trip_collection_manager)
-):
-    tripMgr.clean_collection()
-    return {"message": "Trips collection cleared successfully"}
-
-@router.post("/create-trip", status_code=status.HTTP_201_CREATED)
+@router.post("/trip/create", status_code=status.HTTP_201_CREATED)
 async def create_trip(
+    request: Request,
     trip: Trip, 
-    tripMgr = Depends(get_trip_collection_manager)
+    # tripMgr = Depends(get_trip_collection_manager)
 ):
-    tripMgr.create_trip(trip)
-    return {"message": "Created trip successfully"}
+    user_email = request.session.get('user').get('email')
+    trip.update_owner(user_email)
+    # tripMgr.create_trip(trip)
+
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
+
+# @router.delete("/clear-trips", status_code=status.HTTP_204_NO_CONTENT)
+# async def clear_trips(
+#     tripMgr = Depends(get_trip_collection_manager)
+# ):
+#     tripMgr.clean_collection()
+#     return {"message": "Trips collection cleared successfully"}
     
-@router.get("/query-trip_by_id/{trip_id}", response_model=Trip, status_code=status.HTTP_200_OK)
-async def query_trip_by_id(
-    trip_id: str,
-    tripMgr = Depends(get_trip_collection_manager)
-):
-    tripMgr.query_trip_by_id(trip_id)
+# @router.get("/query-trip_by_id/{trip_id}", response_model=Trip, status_code=status.HTTP_200_OK)
+# async def query_trip_by_id(
+#     trip_id: str,
+#     tripMgr = Depends(get_trip_collection_manager)
+# ):
+#     tripMgr.query_trip_by_id(trip_id)
 
-@router.delete("/delete-trip/{trip_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_trip(
-    trip_id: str,
-    tripMgr = Depends(get_trip_collection_manager)
-):
-    tripMgr.delete_trip_by_id(trip_id)
-    return {"message": "Trip deleted successfully"}
+# @router.delete("/delete-trip/{trip_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_trip(
+#     trip_id: str,
+#     tripMgr = Depends(get_trip_collection_manager)
+# ):
+#     tripMgr.delete_trip_by_id(trip_id)
+#     return {"message": "Trip deleted successfully"}
 
-@router.get("/download-trip/{trip_id}")
-async def download_trip(
-    trip_id: str, 
-    background_tasks: BackgroundTasks,
-    tripMgr = Depends(get_trip_collection_manager),
-    downloader = Depends(get_trip_downloader)
-):
-    file_path = os.path.join(ICS_DIR_PATH, f'trip{trip_id}.ics')
+# @router.get("/download-trip/{trip_id}")
+# async def download_trip(
+#     trip_id: str, 
+#     background_tasks: BackgroundTasks,
+#     tripMgr = Depends(get_trip_collection_manager),
+#     downloader = Depends(get_trip_downloader)
+# ):
+#     file_path = os.path.join(ICS_DIR_PATH, f'trip{trip_id}.ics')
 
-    trip = tripMgr.query_trip_by_id(trip_id)
-    downloader.download_trip(trip)
+#     trip = tripMgr.query_trip_by_id(trip_id)
+#     downloader.download_trip(trip)
 
-    # Define a function to delete the file
-    def delete_temp_file(file_path):
-        os.unlink(file_path)
+#     # Define a function to delete the file
+#     def delete_temp_file(file_path):
+#         os.unlink(file_path)
 
-    # Add the delete_temp_file function as a background task
-    background_tasks.add_task(delete_temp_file, file_path)
+#     # Add the delete_temp_file function as a background task
+#     background_tasks.add_task(delete_temp_file, file_path)
 
-    response = FileResponse(path=file_path, media_type='text/calendar')
-    return response
+#     response = FileResponse(path=file_path, media_type='text/calendar')
+#     return response
