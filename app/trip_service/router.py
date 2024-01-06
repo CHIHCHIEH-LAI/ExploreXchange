68f3dcb6847a99ebbbaecbc00319ff25f1212ae8
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-01-06 14:07:48 +0000

    implement frontend of trip planning

--- app/trip_service/router.py
+++ app/trip_service/router.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, BackgroundTasks, status, Depends
+from fastapi import APIRouter, Request, BackgroundTasks, status, Depends
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
-from typing import List
 import os
 
 from app.common.models.trip import Trip
@@ -9,11 +9,18 @@ from app.trip_service.dependencies import (
     get_trip_downloader
 )
 from app.trip_service.config import ICS_DIR_PATH
+from app.config import FRONTEND_TEMPLATE_DIR
 
 router = APIRouter()
 
+templates = Jinja2Templates(directory=FRONTEND_TEMPLATE_DIR)
+
 # tripMgr = get_trip_collection_manager()
 # downloader = get_trip_downloader()
+
+@router.get("/trip/plan")
+async def plan_trip(request: Request):
+    return templates.TemplateResponse("trip_planning.html", {"request": request})
 
 @router.delete("/clear-trips", status_code=status.HTTP_204_NO_CONTENT)
 async def clear_trips(
