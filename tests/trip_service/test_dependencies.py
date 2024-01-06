0c92e148dd5ac7dc57e44d0da2c6bae8ff733673
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-01-06 23:30:38 +0000

    add trip collection manager unittest

--- tests/trip_service/test_dependencies.py
+++ tests/trip_service/test_dependencies.py
@@ -1,1 +1,10 @@
 import pytest
+
+from app.trip_service.dependencies import get_trip_collection_manager
+
+def test_get_trip_collection_manager():
+    try:
+        tripMgr = get_trip_collection_manager()
+    except Exception as e:
+        assert False, f"An error occurred while getting trip collection manager: {e}"
+    
